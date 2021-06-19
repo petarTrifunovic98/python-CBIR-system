2a7c76e8b16c5ccd9a9b66a53e59f30bc7fc30f4
petarTrifunovic98 <peki.trifunovic@gmail.com>
2021-06-19 12:01:53 +0200

    Added texture features to the inverted index.

--- image_retrieval_service.py
+++ image_retrieval_service.py
@@ -23,11 +23,11 @@ class ImageRetrievalService:
                 img = self.image_processor.resize_image(img)
             if img is not None:
                 vector = self.image_processor.generate_hist_vector(img)
-                # vector = np.concatenate((vector, self.image_processor.generate_texture_vector(img)))
-                discrete_vector = mh.make_vector_discrete(vector[0:6])
+                vector = np.concatenate((vector, self.image_processor.generate_texture_vector(img)))
 
                 vector_sum = np.sum(vector)
                 vector = vector / vector_sum
+                discrete_vector = mh.make_vector_discrete(vector)
                 image = Image(filename, from_dir, img, vector, discrete_vector)
                 self.image_repository.save_image(image)
 
@@ -35,10 +35,10 @@ class ImageRetrievalService:
         img = cv.imread(dir_name + '/' + file_name)
         img = self.image_processor.resize_image(img)
         query_vector = self.image_processor.generate_hist_vector(img)
-        query_vector_discrete = mh.make_vector_discrete(query_vector)
-        # query_vector = np.concatenate((query_vector, self.image_processor.generate_texture_vector(img)))
+        query_vector = np.concatenate((query_vector, self.image_processor.generate_texture_vector(img)))
         query_vector_sum = np.sum(query_vector)
         query_vector = query_vector / query_vector_sum
+        query_vector_discrete = mh.make_vector_discrete(query_vector)
         image = Image(file_name, dir_name, img, query_vector, query_vector_discrete)
 
         similar = self.image_repository.get_similar_images(image)
